18cbc75e6d3f8a49f30817eff80957029c3d42d7
LywwKkA-aD <dr74c.1j8T@proton.me>
2024-12-19 11:28:08 +0100

    [UPDT] news.go: add JSON tags to NewsItem fields for better serialization

--- internal/models/news.go
+++ internal/models/news.go
@@ -1,12 +1,12 @@
+// internal/models/news.go
 package models
 
 import "time"
 
 type NewsItem struct {
-    ID          string
-    Title       string
-    Link        string
-    Description string
-    PubDate     time.Time
-    Categories  []string
+	Title       string    `json:"title"`
+	Link        string    `json:"link"`
+	Description string    `json:"description"`
+	PubDate     time.Time `json:"pub_date"`
+	Categories  []string  `json:"categories"`
 }
